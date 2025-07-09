e5cbeaccb35ef6ca1ca79a8cc67ce9d7594ee3e0
KhushPurohit <148326940+KhushPurohit@users.noreply.github.com>
2025-07-09 21:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ uploaded_file = st.file_uploader("Upload Satellite Image", type=["jpg", "jpeg", 
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     # Preprocess image
     img = image.resize((256, 256))
